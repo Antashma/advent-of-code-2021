6e41c5317a66f0aad6087fdb03809ed2608e9452
Samantha Goodman <antashma@gmail.com>
2021-12-10 22:36:45 -0500

    added code for day 1 part 2 puzzle that does not give correct ans yet

--- scripts/checkIncreasedFromPrev.js
+++ scripts/checkIncreasedFromPrev.js
@@ -12,5 +12,17 @@ function checkIncreasedFromPrev(arr) {
         return increasedFromPrev;
     };
 
-export const res = checkIncreasedFromPrev(data);
-console.log(res);
+    function getThreeMeasurementSums(arr) {
+        let sumArr = []
+        arr.map(
+            function(el, i) {
+                if (arr[i+3]) {
+                    sumArr.push(el + arr[i+1] + arr[i + 2])
+                }
+            }
+        )
+        return sumArr
+    }
+
+export const resDay1Part1 = checkIncreasedFromPrev(data);
+export const resDay1Part2 = checkIncreasedFromPrev(getThreeMeasurementSums(data))
