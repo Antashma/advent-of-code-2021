ee48e1c830d41db95fe368612cd17b7aaa143298
Samantha Goodman <antashma@gmail.com>
2021-12-21 16:20:36 -0500

    added function to make array from input into seperate file

---
+++ scripts/day3-helpers.js
@@ -0,0 +1,3 @@
+import data from "./data-d3.js"
+
+console.log(data)
---
+++ scripts/puzzle-input-to-array.js
@@ -0,0 +1,3 @@
+export default function getArrayFromInput() {
+    return document.getElementById('input').innerText.split(" ");
+}
